a7ea226a828cd90d2e6188b882f6b73c4961c54c
EDGARLOZANO <edg283@gmail.com>
2016-05-04 20:34:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/edgar/ejemplospiiner/principal.java
@@ -0,0 +1,61 @@
+package com.example.edgar.ejemplospiiner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
+;
+
+
+public class principal extends AppCompatActivity {
+
+
+    EditText n1,n2;
+    Button btn;
+    Spinner combo;
+    Switch s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        n1=(EditText)findViewById(R.id.editText);
+        n2=(EditText)findViewById(R.id.editText2);
+        btn=(Button)findViewById(R.id.button);
+        combo=(Spinner)findViewById(R.id.Spinner);
+        s=(Switch)findViewById(R.id.switch1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int a,b;
+                a=Integer.parseInt(n1.getText().toString());
+                b=Integer.parseInt(n2.getText().toString());
+
+                switch (combo.getSelectedItemPosition())
+                {
+                    case 0:
+
+                        Toast.makeText(principal.this,(a+b)+"",Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 1:
+
+                        Toast.makeText(principal.this,(a-b)+"",Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+            }
+        });
+
+
+
+    }
+}
